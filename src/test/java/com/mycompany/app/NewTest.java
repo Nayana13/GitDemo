fc962e70e94b63b28721f5b2272f58352fde79ba
Nayana13 <nayana.mca013@gmail.com>
2019-06-27 17:49:53 -0400

    This is my first commit to git

---
+++ src/test/java/com/mycompany/app/NewTest.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+@Test
+	
+	public void testing() {
+		System.out.println("This is for testing Newtest class");
+	}
+
+}
---
+++ src/test/java/com/mycompany/app/SeleniumTest.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+	@Test
+	
+	public void testing() {
+		System.out.println("This is for testing seleniumtest class");
+	}
+	
+@Test
+	
+	public void testing1() {
+		System.out.println("This is for testing seleniumtest2 class");
+	}
+
+}
